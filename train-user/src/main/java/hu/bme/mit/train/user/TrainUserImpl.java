342db56f4e8ab21c50024c43399ea3de325f5fb5
hbalint12 <36740955+hbalint12@users.noreply.github.com>
2019-03-25 15:20:18 +0100

    Periodic call of followSpeed
    In the constructor set the periodic call of followSpeed.

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -7,9 +7,19 @@ public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
+	private Timer timer;
+	private TimerTask timerTask = new TimerTask() {
+			@Override
+			public void run() {
+				controller.followSpeed();
+			}
+		};
 
 	public TrainUserImpl(TrainController controller) {
 		this.controller = controller;
+		
+		Timer timer = new Timer();
+		timer.scheduleAtFixedRate(timerTask,1000,1000);
 	}
 
 	@Override
